9ba68de4eb079f9080ada824e42e1f14607b9b96
waleed valencia <waleedhd1@hotmail.com>
2020-08-15 06:38:33 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+        <!-- Vendor js -->
+        <script src="assets/js/vendor.min.js"></script>
+        
+        <!-- dropify js -->
+        <script src="assets/libs/dropify/dropify.min.js"></script>
+
+        <!-- form-upload init -->
+        <script src="assets/js/pages/form-fileupload.init.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/app.min.js"></script>
+        
+    </body>
+</html>
